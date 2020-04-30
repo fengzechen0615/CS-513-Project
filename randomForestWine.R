fa22fba0b1b360586c7743106e1de03dfd2d04a9
francisfeng <13903131445@163.com>
2020-04-30 00:10:48 -0400

    Update randomForestWine.R
    "Accuracy Rate is:  69.0562145435792"

--- randomForestWine.R
+++ randomForestWine.R
@@ -15,17 +15,38 @@ idx <- sort(sample(nrow(data), as.integer((.70 * nrow(data)))))
 training <- data[idx, ]
 test <- data[-idx, ]
 
-randomForest <- randomForest(quality~., data=training[, -1], importance=TRUE, ntree=500, na.action=na.roughfix)
+# find the best mtry
+# n <- length(names(training))
+# rate = 1
+# for (i in 1: (n - 1)) {
+#   print(i)
+#   set.seed(100)
+#   rf = randomForest(as.factor(quality)~., data=training, ntree=1000, mtry=i)
+#   rate[i] <- mean(as.numeric(rf$err.rate))
+#   print(rf)
+# }
 
-importance(randomForest)
-# max alcohol 
+# rate
+# plot(rate)
+
+# 0.6631549 0.6101290 0.6119128 0.6112290 0.6072801 0.6086186 0.6064997 0.6072955 0.6089973 0.6084176 0.6109786 0.6101957
+# mtry = 7
+
+# find the best ntree
+# rf <- randomForest(as.factor(quality)~., data=training, mtry=7, importance=TRUE, ntree=1000)
+# plot(rf)
+
+# when trees equal 800, the model is becoming steady.
+
+randomForest <- randomForest(as.factor(quality)~., data=training, mtry=7, importance=TRUE, ntree=500, proximity=TRUE)
+
+# importance(randomForest)
+# max alcohol
 # min type
 
-varImpPlot(randomForest)
-plot(randomForest)
-MDSplot(randomForest, data$quality)
+# varImpPlot(randomForest)
 
-randomForestPrediction <- round(predict(randomForest, test), 0)
+randomForestPrediction <- predict(randomForest, test)
 
 table(actual=test$quality, Prediction=randomForestPrediction)
 
