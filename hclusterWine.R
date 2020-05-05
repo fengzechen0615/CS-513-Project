8a2e0769e0f1ac6a739c5ed368c439b43607ffde
SuperLouV <39430745+SuperLouV@users.noreply.github.com>
2020-05-05 01:21:12 -0400

    Add files via upload

--- hclusterWine.R
+++ hclusterWine.R
@@ -9,10 +9,12 @@ dev.off
 #read CSV
 #setwd("/Users/louyilin/RStudioProjects/FinalProject")
 hData<-read.csv("winequality-red.csv",header = TRUE,na.strings = "?")
-
+standardize <- function(x){x/max(x)} 
 #  clean NA datas
 hData=na.omit(hData)
 #hData$type <- as.numeric(hData$type, levels = c("white", "red"), labels = c("1","2"))
+hData[,-12] <- as.data.frame(lapply(hData[,-12], standardize))
+hData[,12]=as.factor(hData[,12])
 hCluster=hclust(dist(hData[,-c(12)]),method="average")
 #divide into 2 clusters
 plot(hCluster)
--- kmeansWine.R
+++ kmeansWine.R
@@ -13,11 +13,11 @@ dev.off
 #setwd("/Users/louyilin/RStudioProjects/FinalProject")
 #knnData<-read.csv("winequalityN.csv",header = TRUE,na.strings = "?")
 knnData<-read.csv("winequality-red.csv",header = TRUE,na.strings = "?")
-
+standardize <- function(x){x/max(x)} 
 #  clean NA datas
 knnData=na.omit(knnData)
 #knnData$type <- as.numeric(knnData$type, levels = c("white", "red"), labels = c("1","2"))
-
+knnData[,-12] <- as.data.frame(lapply(knnData[,-12], standardize))
 kMean=kmeans(knnData[,-c(12)],3)
 plot(knnData[c(3,8)],col=kMean$cluster)
 points(kMean$centers,col=1:3,pch=16,cex=2)
